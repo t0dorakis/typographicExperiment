8bed64becdc8e560882ddb4c815eebacc9b3de15
t0dorakis <theo.hillmann@gmail.com>
2019-06-15 20:22:04 +0200

    optimized for video mockup

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@
 
 // Load application styles
 import 'styles/index.scss';
+
 // import { load } from 'opentype.js';
 
 // var opentype = require('opentype.js');
@@ -13,85 +14,31 @@ import * as opentype from '../node_modules/opentype.js/dist/opentype.js';
 // ================================
 // START YOUR APP HERE
 // ================================
-
 //TODO: outsource text
 window.onload = function () {
-    var dreckText = "\n—— ——— ——— ——— —— ——— ——— ————— ——— ——— ————— ——— ——— ————— ——— ——— ————— ——— ——— ———\n" +
-        "Sehen sie den Dreck? fragte er. Nicht nur in den Ritzen, nicht nur versteckt hinterm\n" +
-        "Schrank usf. sozusagen nicht nur im Geheimen. Nein, ganz offensichtlich liegt er\n" +
-        "hier... in all seiner grausamen Unendlichkeit. Wir k\u00e4mpfen ja seit je her gegen den\n" +
-        "Dreck und im Grunde mittlerweile eigentlich nur noch gegen den Staub und den Dreck,\n" +
-        "wie er ganz offensichtlich ganz von alleine, ohne jedwedes Zutun immer\n" +
-        "und immer wieder \u00fcberall liegt. Man wischt. Man wischt ja im Grunde immerzu und\n" +
-        "der Dreck kommt gleich wieder. Ich wische \u00fcber den Flurboden und denke: jetzt ist\n" +
-        "der Dreck weg... aber wissen Sie... der Dreck ist eigentlich nicht weg, ist nie weg\n" +
-        "und selbst wenn wir fest davon \u00fcberzeugt sind, der Dreck sei beseitigt, sei fort, ist\n" +
-        "der Dreck noch immer da und vor allem nach k\u00fcrzester Zeit auch wieder\n" +
-        "offensichtlich da. Heute haben wir mehr Dreck als fr\u00fcher, auch wenn das alle\n" +
-        "abstreiten. Die Leute streiten ja ab, dass wir heute mehr Dreck als fr\u00fcher haben\n" +
-        "und behaupten allen Ernstes, der Dreck habe sich verringert, w\u00e4hrenddessen man\n" +
-        "zugeben muss, dass der Dreck sich im Grunde nur verschlimmert habe. Sehen sie,\n" +
-        "ich wische hier \u00fcber den Tisch und verschmiere den Dreck eigentlich nur. Der\n" +
-        "Lumpen saugt den Dreck gar nicht mehr auf, sondern verwischt ihn bestenfalls,\n" +
-        "schlimmstenfalls aber hat der Lumpen auf den Dreck \u00fcberhaupt keine Wirkung. \n" +
-        "\n" +
-        "     Wir alle ersticken ja am Dreck, f\u00fchren ein dreckiges Leben im Dreck und in\n" +
-        "Kooperation mit dem Dreck, mit dem wir uns abgefunden haben. Daher erscheint\n" +
-        "uns der Dreck nicht mehr bedeutend. Der Dreck aber ist das absolut bedeutenste\n" +
-        "und war es immer. Ich wische also und wische und der Dreck ist nach der\n" +
-        "k\u00fcrzesten Zeit einfach wieder da... Man erstickt ja im Dreck. Man wischt ja im\n" +
-        "Grunde nur noch gegen den Schmutz an und nennt das Leben. Es wird nichts\n" +
-        "mehr geschaffen, sondern nur wiederhergestellt... aber im Kern verfault alles und\n" +
-        "am Ende hat man nichts. Man k\u00e4mpft ja um die eigene Existenz, also gegen den\n" +
-        "Schmutz aber der Schmutz gewinnt immer und wir f\u00fchren eine f\u00fcrchterliche\n" +
-        "Existenz im Dreck oder versuchen am Dreck vorbei zu leben. Ich kann aber nicht\n" +
-        "am Dreck vorbei leben und lebe dadurch jeden Tag f\u00fcr den Dreck. EIn Leben f\u00fc̈r\n" +
-        "den Schmutz! Das solle ich mir mal vorstellen, sagte er lachend. Wir stehen ja\n" +
-        "morgens fr\u00fch auf f\u00fcr den Dreck. Wischen hier und dort, versuchen uns am Leben\n" +
-        "zu halten, f\u00fcr den Dreck, dass wir ihn wischen können, also im Grunde gegen den\n" +
-        "Dreck. Man lebt nicht mehr f\u00fcr etwas, man lebt im Grunde nur noch gegen etwas\n" +
-        "und dieses Etwas, das ist der Dreck. Man lebt quasi gegen den Dreck an und in\n" +
-        "jeden neuen Tag hinein und immer f\u00fcr den Schmutz in den Ritzen, im Spalt, in den\n" +
-        "Löchern usf. aber auch auf der Fl\u00e4che, sagte er. Im Grunde aber mache man sich\n" +
-        "ja zum Affen f\u00fcr den Dreck. Das sei eine entsetzliche Vorstellung, wie er finde...\n" +
-        "ein Affe des Drecks zu sein. Es stink immer, sagte er. Es stinkt drinnen, es\n" +
-        "stinkt draußen... da könne man sich das L\u00fcften gleich sparen und immer im Kampfe\n" +
-        "gegen den Dreck und immer im \u00dcberlebenskampfe gegen den Erstickungstod.\n" +
-        "Wolle er jede Ecke, jeden Winkel abstauben und das t\u00e4glich, gr\u00fcndlich, br\u00e4uchte er\n" +
-        "am Tage mehr Stunden als einem der Tag \u00dcberhaupt naturgemäß bereitstellt. Die\n" +
-        "Einrichtungen werden ja immer reduzierter. Hier ein Schemel, da noch ein Tisch,\n" +
-        "darauf eine Vase. Das muss heutzutage reichen, wo der Dreck unbesiegbar\n" +
-        "geworden ist. Der Minimalismus und Purismus, das seien ja Gegenmaßnahmen,\n" +
-        "seien Versuche gegen den Dreck... aber der Dreck gewinnt immer. Wo man\n" +
-        "hinkommt, es wimmelt der Schmutz und alles ist mir ekelhaft geworden und ich\n" +
-        "verlasse das Haus kaum noch... habe meine ganze Existenz ins Aufwischen\n" +
-        "geworfen, hinein in den Dreck geworfen, dem Dreck alles gewidmet und geopfert...\n" +
-        "aber auch um zu \u00dcberleben.\n" +
-        "—— ——— ——— ——— —— ——— ——— ————— ——— ——— ————— ——— ——— ————— ——— ——— ————— ——— "
-    var nameText = " SOEREN MEYERHOFER"
-    var titelText = " -DER DRECK-"
+
+    const textContainer = document.getElementById('textContainerMobile');
+    const dreckText = textContainer.textContent;
+    const nameText = " SOEREN MEYERHOFER"
+    const titelText = " DER DRECK"
 
     //Split text into Words/Letters
-    var TextArray = dreckText.split("\n")
-    var NameArray = nameText.split("")
-    var titelArray = titelText.split("")
+    const TextArray = dreckText.split("\n")
+    const NameArray = nameText.split("")
+    const titelArray = titelText.split("")
 
     //TODO: Make Responisve
     // Viewheight variables
-    var width = window.innerWidth / 2
-    var textHeight = window.innerheight / 200;
+    const width = window.innerWidth / 2
+    const textHeight = window.innerheight / 300;
 
     //TODO: did could be done in the index.html
    //HZML Elemnts
-    var sideBar = document.createElement('div');
-    sideBar.className = 'sidebar';
-    document.getElementsByTagName('row')[0].appendChild(sideBar);
+    const sideBar = document.getElementById('artist');
 
-    var headBar = document.createElement('div');
-    headBar.className = 'container';
-    document.getElementsByTagName('header')[0].appendChild(headBar);
+    const headline = document.getElementById('headline');
 
-    var iDiv = document.createElement('div');
+    const iDiv = document.createElement('div');
     iDiv.className = 'container';
     document.getElementsByTagName('row')[0].appendChild(iDiv);
 
@@ -121,7 +68,6 @@ window.onload = function () {
         return yM;
     }
 
-    var font = "";
 //
 
     // snapVariables
@@ -139,7 +85,7 @@ window.onload = function () {
         innerDiv.className = 'snapGrid';
         innerDiv.id = "inner" + index;
         innerDiv.setAttribute('width', '' + width*4  + '');
-        innerDiv.setAttribute('height', '55px');
+        innerDiv.setAttribute('height', '50px');
         // The variable iDiv is still good... Just append to it.
         iDiv.appendChild(innerDiv);
     });
@@ -161,7 +107,7 @@ window.onload = function () {
         innerDiv.setAttribute('width', '' + width / 7  + '');
         innerDiv.setAttribute('height', '220px');
         // The variable iDiv is still good... Just append to it.
-        headBar.appendChild(innerDiv);
+        headline.appendChild(innerDiv);
     });
 
     function onMouseUpdate(e) {
@@ -225,7 +171,6 @@ window.onload = function () {
                 doSnap(snapPath,index, xedni);
                 var snapCtx = document.getElementById('inner' + index).getContext('2d');
                 snapCtx.setTransform(1, 0, 0, 1, 0, 0);
-                console.log(" xedni " + xedni)
                 snapCtx.clearRect(0, 0, width*4*getMouseY()/index*5, getMouseX()*index);
                 snapPath.draw(snapCtx);
             })
